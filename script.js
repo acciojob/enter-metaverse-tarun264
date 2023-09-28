b0225977c304f6ebc7f8a269f7932432e71cb449
Tarun Rana <66361151+tarun264@users.noreply.github.com>
2023-09-28 14:41:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const para=document.getElementById("status");
+const btn= document.getElementById("enterBtn");
+
+btn.addEventListener("click", ()=>{
+	para.innerHTML="<h1>Entered Metaverse</h1>";
+})
